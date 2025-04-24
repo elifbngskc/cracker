18befa6fe768ccb5465856699b058cbf0009852b
Hakurei <159775249+atelieralice@users.noreply.github.com>
2025-04-24 16:15:54 +0300

    migrate sqlite to postgres

--- cracker/settings.py
+++ cracker/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'cracker.wsgi.application'
 #}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # Replace with your database name
+        'USER': 'postgres',  # Replace with your database username
+        'PASSWORD': 'postgres',  # Replace with your database password
+        'HOST': 'db',  # Replace with your database host (use 'db' if using Docker)
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
 
