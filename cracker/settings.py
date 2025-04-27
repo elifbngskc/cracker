4565ad2fccf0772ef2fe60ee3ff85976f77215e3
elifbngskc <istekuzukuzugel@gmail.com>
2025-04-27 20:33:04 +0300

    added redis cache

--- cracker/settings.py
+++ cracker/settings.py
@@ -156,3 +156,13 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'tracker:home'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
