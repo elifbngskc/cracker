f9c5a5cf28a5d48b52991e04d65068e68ce0e18e
Hakurei <159775249+atelieralice@users.noreply.github.com>
2025-04-27 17:16:01 +0300

    added a form to test the API

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    calories = models.FloatField(null=True, blank=True)
+    carbohydrates = models.FloatField(null=True, blank=True)
+    fats = models.FloatField(null=True, blank=True)
+    proteins = models.FloatField(null=True, blank=True)
+    fiber = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
